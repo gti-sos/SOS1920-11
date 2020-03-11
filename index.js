94983a29e75dbc510811adc936c6ed35c4797612
IgnacioCVGitHub <icalcedo.09play@gmail.com>
2020-03-11 22:16:55 +0000

    Terminado el despliegue del servidor

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ var app = express();
 
 var tiempo= new Date()
 
-app.get("/time",(request,response)=>{
-	response.sendFile("/hora.html", { root: __dirname });
+app.get("/public",(request,response)=>{
+	response.sendFile("/public/index.html", { root: __dirname });
 });
+
+
 app.listen(80);
 
 console.log("iniciando servidor...")
