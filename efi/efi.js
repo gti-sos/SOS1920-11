f83e86427dfef3f235e3385cb060921f4a725f26
IgnacioCVGitHub <icalcedo.09play@gmail.com>
2020-03-25 17:12:28 +0000

    corregido uso de bodyparser para trabajar con PUT

--- efi/efi.js
+++ efi/efi.js
@@ -3,7 +3,8 @@ const express = require("express");
 var app = express();
 var router = express.Router();
 module.exports=router;
-//const bodyParser = require("body-parser");
+const bodyParser = require("body-parser");
+app.use(bodyParser.json()); 
 
 const BASE_API_URL = "/api/v1";
 
@@ -35,13 +36,13 @@ router.post('/',(req,res) =>{
 	if((newefi == "") || (newefi.country == null)){
 		res.sendStatus(400,"BAD REQUEST");
 	} else {
-		efis.push(newContact); 	
+		efis.push(newefi); 	
 		res.sendStatus(201,"CREATED");
 	}
 });
 
 //get efi
-app.get("/:country", (req,res)=>{
+router.get("/:country", (req,res)=>{
 	
 	var country = req.params.country;
 	
@@ -59,7 +60,7 @@ app.get("/:country", (req,res)=>{
 
 //delete specific efi
 
-app.delete("/:country", (req,res)=>{
+router.delete("/:country", (req,res)=>{
 	
 	var country = req.params.country;
 	
@@ -74,6 +75,20 @@ app.delete("/:country", (req,res)=>{
 	}else{
 		res.sendStatus(404,"COUNTRY NOT FOUND");
 	}
+});
+
+//PUT specific efi
+
+router.put('/:country', (req,res)=>{
 	
+	var country= req.params.country;
+	var efisfiltro = efis.filter((c) => {
+		return (c.country == country);
+	});
 	
+	if (efisfiltro.length==0){
+		res.sendStatus(404,"COUNTRY NOT FOUND");	
+	}else if(req.body){
+			 
+	}
 });
