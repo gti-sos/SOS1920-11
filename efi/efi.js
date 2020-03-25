b8cf2d6b550a4e206df445f039ca6434a49ccdd4
IgnacioCVGitHub <icalcedo.09play@gmail.com>
2020-03-25 10:22:13 +0000

    avance en get, post y delete de los efis

--- efi/efi.js
+++ efi/efi.js
@@ -14,14 +14,66 @@ var efis=[
 	{
 		'country':'New Zeland'
 	}
-	
 ]
+//route handler
 router.use(function procesador(req,res,next){
 	console.log('Tratamiento de los efi');
 	next();
 });
+
+//get efis
 router.get('/', function(req,res,next){
 	
 	res.send(efis);
 	res.sendStatus(200,'OK')
 })
+//post efis
+router.post('/',(req,res) =>{
+	
+	var newefi = req.body;
+	
+	if((newefi == "") || (newefi.country == null)){
+		res.sendStatus(400,"BAD REQUEST");
+	} else {
+		efis.push(newContact); 	
+		res.sendStatus(201,"CREATED");
+	}
+});
+
+//get efi
+app.get("/:country", (req,res)=>{
+	
+	var country = req.params.country;
+	
+	var efisfiltro = efis.filter((c) => {
+		return (c.country == country);
+	});
+	
+	
+	if(efisfiltro.length >= 1){
+		res.send(efisfiltro[0]);
+	}else{
+		res.sendStatus(404,"COUNTRY NOT FOUND");
+	}
+})
+
+//delete specific efi
+
+app.delete("/:country", (req,res)=>{
+	
+	var country = req.params.country;
+	
+	var efisfiltro = efis.filter((c) => {
+		return (c.country != country);
+	});
+	
+	
+	if(efisfiltro.length < efis.length){
+		efis = efisfiltro;
+		res.sendStatus(200);
+	}else{
+		res.sendStatus(404,"COUNTRY NOT FOUND");
+	}
+	
+	
+});
