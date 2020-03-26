8f5b803896814b8cefe99e51fdda54ffba03ee48
IgnacioCVGitHub <icalcedo.09play@gmail.com>
2020-03-26 13:20:30 +0000

    Cambios en la estructura de datos de los efi

--- efi/efi.js
+++ efi/efi.js
@@ -5,7 +5,7 @@ var router = express.Router();
 module.exports=router;
 const bodyParser = require("body-parser");
 app.use(bodyParser.json()); 
-const parametros= 3;
+const parametros= 15;
 const BASE_API_URL = "/api/v1";
 
 
@@ -24,13 +24,37 @@ router.get('/loadInitialData',(req,res)=>{
 	var init=[{
 		'country':'New Zeland',
 		'year':2019,
-		'efiindex':84.4
-},
+		'efiindex':84.4,
+		'efigovint':95,
+		'efipropright':96.7,	
+		'efijudefct':83.5,	
+		'efitaxburden':71,	
+		'efigovspend':50.4,	
+		'efisicalhealth':98.6, 	
+		'efibusfreed':91,	
+		'efilabfreed':86.7,
+		'efimonfreed':87.5,	
+		'efitradefreed':92.4,	
+		'efiinvfreed':80,	
+		'efifinfred':80
+	},
 {
 		'country':'Chile',
 		'year': 2019,
-		'efiindex':75.4
-}];
+		'efiindex':75.4,
+		'efigovint':62.3,
+		'efipropright':68.7,	
+		'efijudefct':56.3,	
+		'efitaxburden':77.3,	
+		'efigovspend':81,	
+		'efisicalhealth':89, 	
+		'efibusfreed':76.6,	
+		'efilabfreed':65,
+		'efimonfreed':84.5,	
+		'efitradefreed':88.8,	
+		'efiinvfreed':85,	
+		'efifinfred':70
+		}];
 	efis=init;
 	res.sendStatus(201,"DATA CREATED");
 });
@@ -117,6 +141,18 @@ router.put('/:country', (req,res)=>{
 					c.country=body["country"];
 					c.year=body["year"];
 					c.efiindex=body["efiindex"];
+					c.efigovint=body["efigovint"];
+					c.efipropright=body["efipropright"];
+					c.efijudefct=body['efijudefct'];
+					c.efitaxburden=body['efitaxburden'];
+					c.efigovspend=body['efigovspend'];
+					c.efisicalhealth=body['efisicalhealth'];
+					c.efibusfreed=body['efibusfreed'];
+					c.efilabfreed=body['efilabfreed'];
+					c.efimonfreed=body['efimonfreed'];
+					c.efitradefreed=body['efitradefreed'];
+					c.efiinvfreed=body['efiinvfreed'];
+					c.efifinfred=body['efifinfred'];
 				}
 			});
 			res.sendStatus(200,"OK");
