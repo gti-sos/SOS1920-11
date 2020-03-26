23c3f2215640f73e155b14d494fcc6883389d9cd
arh09 <arubioh94@gmail.com>
2020-03-26 00:48:32 +0000

    added API for rents per capita, issues left

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 var app = express();
+const BASE_API_URL = "/api/v1";
 
 app.use(bodyParser.json());
 
@@ -13,115 +14,25 @@ app.get("/public",(request,response)=>{
 
 app.use("/",express.static("./public"));
 
-var rpcs = [
-	{ 
-		country: "Spain",
-		vpy: 2.0	
-	},
-	{ 
-		country: "Slovenia",
-		vpy: 2.4
-	}
-];
-
-const BASE_API_URL = "/api/v1";
-
-// GET RPCS
-
-app.get(BASE_API_URL+"/rpcs", (req,res) =>{
-	res.send(JSON.stringify(rpcs,null,2));
-	console.log("Data sent:"+JSON.stringify(rpcs,null,2));
-});
-
-
-// POST RPCS
-
-app.post(BASE_API_URL+"/rpcs",(req,res) =>{
-	
-	var newRpc = req.body;
-	
-	if((newRpc == "") || (newRpc.country == null)){
-		res.sendStatus(400,"BAD REQUESTT");
-	} else {
-		rpcs.push(newRpc); 	
-		res.sendStatus(201,"CREATED");
-	}
-});
-
-// DELETE RPCS
-
-app.delete(BASE_API_URL+"/rpcs", (req,res)=>{
-	
-	
-	
-	var filteredRpcs = rpcs.filter((c) => {
-		return (c.name != null);
-	});
-	
-	
-	if(filteredRpcs.length == 0){
-		rpcs = filteredRpcs;
-		res.sendStatus(200);
-	}else{
-		res.sendStatus(400,"COUNTRY NOT FOUND");
-	}
-	
-	
-});
-
-
-// GET RPCS/XXX
-
-app.get(BASE_API_URL+"/rpcs/:country", (req,res)=>{
-	
-	var country = req.params.country;
-	
-	var filteredRpcs = rpcs.filter((c) => {
-		return (c.country == country);
-	});
-	
-	
-	if(filteredRpcs.length >= 1){
-		res.send(filteredRpcs[0]);
-	}else{
-		res.sendStatus(404,"COUNTRY NOT FOUND");
-	}
-});
-
-// PUT RPCS/XXX
-
-// DELETE RPCS/XXX
-
-app.delete(BASE_API_URL+"/rpcs/:country", (req,res)=>{
-	
-	var country = req.params.country;
-	
-	var filteredRpcs = rpcs.filter((c) => {
-		return (c.country != country);
-	});
-	
-	
-	if(filteredRpcs.length < rpcs.length){
-		rpcs = filteredRpcs;
-		res.sendStatus(200);
-	}else{
-		res.sendStatus(404,"COUNTRY NOT FOUND");
-	}
-	
-	
-});
 
 //Acceso al recurso economic-freedom-indexes
 
-rutaEFI = BASE_API_URL + '/economic-freedom-indexes'; 
+const rutaEFI = BASE_API_URL + '/economic-freedom-indexes'; 
 var efis=  require('./efi/efi');
 app.use(rutaEFI,efis);
 
 //Acceso a crime-rate-stats
 
-rutaCrime = BASE_API_URL + '/crime-rate-stats';
+const rutaCrime = BASE_API_URL + '/crime-rate-stats';
 var rcrime=  require('./crime/API_jose');
 app.use(rutaCrime,rcrime);
+
+//Acceso a rents-per-capita
+
+const rutaRPC = BASE_API_URL + '/rents-per-capita';
+var rpcs=  require('./rpc/API_RPC');
+app.use(rutaRPC,rpcs);
+
 
 app.listen(port, () => {
 	console.log("Server ready");
