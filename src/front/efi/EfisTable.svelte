89ad61d1bfe5b27a50fbd6bc92786e018030aad4
IgnacioCVGitHub <icalcedo.09play@gmail.com>
2020-05-05 17:05:37 +0200

    continuación de implementacion del frontend de los efis

---
+++ src/front/efi/EfisTable.svelte
@@ -0,0 +1,32 @@
+<script>
+import {onMount} from "svelte";
+import Table from "sveltestrap/src/Table.svelte";
+import Button from "sveltestrap/src/Button.svelte";
+
+let efis=[];
+let newEfi={
+    country:"",
+    year:"",
+    efiindex:"",
+    efigovint:"",
+    efipropright:"",
+    efijudefct:"",
+    efitaxburden:"",
+    efigovspend:"",
+    efisicalhealth:"",
+    efibusfreed:"",
+    efilabfreed:"",
+    efimonfreed:"",
+    efitradefreed:"",
+    efiinvfreed:"",
+    efifinfred:""
+};
+onMount(getEfis);
+
+</script>
+
+
+<main>
+
+
+</main>
