236d9a5c7d03496fd86ce4bf5227816933174e81
IgnacioCVGitHub <icalcedo.09play@gmail.com>
2020-05-05 18:10:19 +0200

    3o fix a los efis. hay problemas graves con la aplicación. intentando revertir los cambios

--- src/back/efi/efi.js
+++ src/back/efi/efi.js
@@ -21,8 +21,44 @@ router.use(function procesador(req, res, next) {
 });
 
 //loadInitialData
-import initialdata from 'initialdata.js';
-var init = initialdata;
+//import initialdata from 'initialdata.js';
+var init = [
+	{
+		country: 'New Zeland',
+		year: 2019,
+		efiindex: 84.4,
+		efigovint: 95,
+		efipropright: 96.7,
+		efijudefct: 83.5,
+		efitaxburden: 71,
+		efigovspend: 50.4,
+		efisicalhealth: 98.6,
+		efibusfreed: 91,
+		efilabfreed: 86.7,
+		efimonfreed: 87.5,
+		efitradefreed: 92.4,
+		efiinvfreed: 80,
+		efifinfred: 80
+	},
+	{
+		country: 'Chile',
+		year: 2019,
+		efiindex: 75.4,
+		efigovint: 62.3,
+		efipropright: 68.7,
+		efijudefct: 56.3,
+		efitaxburden: 77.3,
+		efigovspend: 81,
+		efisicalhealth: 89,
+		efibusfreed: 76.6,
+		efilabfreed: 65,
+		efimonfreed: 84.5,
+		efitradefreed: 88.8,
+		efiinvfreed: 85,
+		efifinfred: 70
+	}
+]
+;
 
 function isEmpty(obj) {
 	for (var key in obj) {
