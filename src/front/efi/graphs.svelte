4465caf5edaa300e68e1f0dc55dff51d35eeaa14
IgnacioCVGitHub <icalcedo.09play@gmail.com>
2020-05-27 19:23:16 +0200

    fixed errors of plotting with ploty

--- src/front/efi/graphs.svelte
+++ src/front/efi/graphs.svelte
@@ -34,7 +34,7 @@ function dataParser(arrayofData){
         let nombre="Año "+ years[k];
         data_ploty.push(
             {
-             histfunc: nombre,
+             histfunc: "sum",
                 y: efis_2,
                 x: countries,
                 type: "histogram",
@@ -121,43 +121,7 @@ async function cargaGraph(){
 <script src="https://code.highcharts.com/modules/accessibility.js" ></script>
 <script src='https://cdn.plot.ly/plotly-latest.min.js' on:load="{cargaGraph}"></script>
 <style>
-.highcharts-figure, .highcharts-data-table table {
-    min-width: 310px; 
-    max-width: 800px;
-    margin: 1em auto;
-}
 
-#container {
-    height: 400px;
-}
-
-.highcharts-data-table table {
-	font-family: Verdana, sans-serif;
-	border-collapse: collapse;
-	border: 1px solid #EBEBEB;
-	margin: 10px auto;
-	text-align: center;
-	width: 100%;
-	max-width: 500px;
-}
-.highcharts-data-table caption {
-    padding: 1em 0;
-    font-size: 1.2em;
-    color: #555;
-}
-.highcharts-data-table th {
-	font-weight: 600;
-    padding: 0.5em;
-}
-.highcharts-data-table td, .highcharts-data-table th, .highcharts-data-table caption {
-    padding: 0.5em;
-}
-.highcharts-data-table thead tr, .highcharts-data-table tr:nth-child(even) {
-    background: #f8f8f8;
-}
-.highcharts-data-table tr:hover {
-    background: #f1f7ff;
-}
 </style>
 </svelte:head>
 <main>
