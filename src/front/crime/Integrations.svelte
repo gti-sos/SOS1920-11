4888c78855c1b0767622f230d018494ea271e226
JaredYeeto <jcalcedo.vazquez@gmail.com>
2020-05-30 14:31:41 +0200

    comienzo integraciones

---
+++ src/front/crime/Integrations.svelte
@@ -0,0 +1,22 @@
+<script>
+
+    import {pop} from "svelte-spa-router";
+	import Button from "sveltestrap/src/Button.svelte";
+</script>
+
+<main>
+<h2>
+		Integrations:
+		</h2> 
+		<ul>
+			<li><a href="/#/efis/integrations/gini_index">Índice GINI</a>
+			<li><a href="/#/efis/integrations/cryptocurrency">Criptomonedas</a>
+			<li><a href="/#/efis/integrations/beerimports">Importación de alcohol</a>
+			<li><a href="/#/efis/integrations/contamination">Contaminación por producción de vehiculos</a>
+			<li><a href="/#/efis/integrations/icm">ICM </a>
+			
+		</ul>
+
+	<a href="/#"><Button outline color="warning">INICIO</Button></a>
+    <Button outline color="secondary" on:click="{pop}">VOLVER</Button>
+</main>
