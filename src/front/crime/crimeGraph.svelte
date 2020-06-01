6a3c4fdad8c04642396d94995115f7da443dcc78
JaredYeeto <jcalcedo.vazquez@gmail.com>
2020-06-01 14:07:58 +0200

    empieza grafo crimes

---
+++ src/front/crime/crimeGraph.svelte
@@ -0,0 +1,40 @@
+<script>
+    Highcharts.chart('container', {
+    data: {
+        table: 'datatable'
+    },
+    chart: {
+        type: 'column'
+    },
+    title: {
+        text: 'Data extracted from a HTML table in the page'
+    },
+    yAxis: {
+        allowDecimals: false,
+        title: {
+            text: 'Units'
+        }
+    },
+    tooltip: {
+        formatter: function () {
+            return '<b>' + this.series.name + '</b><br/>' +
+                this.point.y + ' ' + this.point.name.toLowerCase();
+        }
+    }
+});
+</script>
+<svelte:head>
+    <script src="https://code.highcharts.com/highcharts.js"></script>
+    <script src="https://code.highcharts.com/modules/exporting.js"></script>
+    <script src="https://code.highcharts.com/modules/export-data.js"></script>
+    <script src="https://code.highcharts.com/modules/accessibility.js" ></script>
+</svelte:head>
+<main>
+    <figure class="highcharts-figure">
+    <div id="container"></div>
+    <p class="highcharts-description">
+        En este gráfico podemos apreciar la tasa de criminalidad y de seguridad de varios países
+        dadas por el índice de asesinatos y el de robos por país.
+    </p>
+
+</main>
