2f21522f7ce4acfc4642a47f9d16a6cd160f1670
JaredYeeto <jcalcedo.vazquez@gmail.com>
2020-06-02 23:15:25 +0200

    encuentro libreria 2 para grafico y trabajo

--- src/front/crime/crimeGraph.svelte
+++ src/front/crime/crimeGraph.svelte
@@ -1,6 +1,55 @@
 <script>
 
-    import {onMount} from 'svelte';
+import {onMount} from 'svelte';
+
+async function cargaLabels(){
+
+    const resData = await fetch("/api/v3/rents-per-capita");
+    datos = await resData.json;
+
+}
+
+var Chart = require('chart.js');
+var ctx = document.getElementById('myChart');
+var myChart = new Chart(ctx,{
+    type: 'bar',
+    data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+            label: 'Crime rate stats',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+});
+
+
+
 
     Highcharts.chart('container', {
     data: {
@@ -33,11 +82,13 @@
     <script src="https://code.highcharts.com/modules/accessibility.js" ></script>
 </svelte:head>
 <main>
+    <h2>Crime rate stats - Highcharts</h2>
     <figure class="highcharts-figure">
     <div id="container"></div>
     <p class="highcharts-description">
         En este gráfico podemos apreciar la tasa de criminalidad y de seguridad de varios países
         dadas por el índice de asesinatos y el de robos por país.
     </p>
-
+    <h2>Crime rate stats - Highcharts</h2>
+    <canvas id="myChart" width="400" height="400"></canvas>
 </main>
