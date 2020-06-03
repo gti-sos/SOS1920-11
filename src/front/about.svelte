5986ec770dd4ca27e933d8ebc2616a94359608b5
IgnacioCVGitHub <icalcedo.09play@gmail.com>
2020-06-03 22:55:08 +0200

    Added list of links to the videos

--- src/front/about.svelte
+++ src/front/about.svelte
@@ -1,3 +1,18 @@
+<script>
+let linkEFIS= "https://youtu.be/pHpb6nUrVC8";
+</script>
 <main>
-<iframe title="Economic Freedom Indexes" src="https://youtu.be/pHpb6nUrVC8"></iframe>
+
+<h1>Sobre nuestras APIS</h1>
+<p>
+Aquí podrás encontrar vídeos de cada una de las distintas APIS del proyecto SOS1920-11.
+Los enlaces llevarán a videos de Youtube ocultos donde informarse al respecto:
+</p>
+<ul>
+<li><a href={linkEFIS} target="_blank">Economic Freedom Indexes</a></li>
+<li><a>Crime Rates Stats</a></li>
+<li><a>Rents per Capita</a></li>
+</ul>
+
+
 </main>
