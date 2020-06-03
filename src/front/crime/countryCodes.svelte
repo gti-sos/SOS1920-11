d6b7d9cfbbff62a11e59bb113ce5ab053c298572
JaredYeeto <jcalcedo.vazquez@gmail.com>
2020-06-04 00:40:29 +0200

    integraciones ajenas v1

--- src/front/crime/countryCodes.svelte
+++ src/front/crime/countryCodes.svelte
@@ -1,27 +1,27 @@
 <script>  
 
  async function cargadatos() {
-    const resData = await fetch("https://iterar-mapi-us.p.rapidapi.com/api/reserpine/doses.json", {
+    const resData = await fetch("https://geohub3.p.rapidapi.com/countries", {
 	    "method": "GET",
 	    "headers": {
-		"x-rapidapi-host": "iterar-mapi-us.p.rapidapi.com",
-	    "x-rapidapi-key": "3bccf248c4msh3d4a9dedc2c020cp1a889bjsn31014b11cae5"
+	"x-rapidapi-host": "geohub3.p.rapidapi.com",
+	"x-rapidapi-key": "3bccf248c4msh3d4a9dedc2c020cp1a889bjsn31014b11cae5"
         }});
         
     let data = await resData.json();
     let data_ploty=[];
-    let drugs=[];
-    let dosis=[];
+    let paises=[];
+    let codigos=[];
     data.forEach(element => {
-        drugs.push(element.name);
-        dosis.push(element[""]);
+        paises.push(element.name);
+        codigos.push(element["countryCode"]);
     });
     data_ploty.push({
         histfunc: "sum",
-        y: dosis,
-        x: drugs,
+        y: codigos,
+        x: paises,
         type: "histogram",
-        name: "Dosis de drogas"
+        name: "Códigos de países"
     });
     Plotly.newPlot('myDiv', data_ploty);
 }
@@ -30,9 +30,9 @@
 <script src='https://cdn.plot.ly/plotly-latest.min.js' on:load="{cargadatos}"></script>
 </svelte:head>
 <main>
-<h1>Dosis de reserpina</h1>
+<h1>Códigos de cada país</h1>
 <div>
-    En esta gráfica se representan las dosis de la reserpina
+    En esta gráfica se representan las codigos de cada país en el mundo
 <br>
 <div id="myDiv"></div>
 </div>
--- src/front/crime/population.svelte
+++ src/front/crime/population.svelte
@@ -3,37 +3,30 @@ let CountriesData = [];
 let chartData =[];
 var nun = 0;
 var bb = 0;
-async function cuentaAeros(p, a){
-    for (i in a) {
-        if (i.nameCountry == p.nameCountry){
-            bb = bb +1;
-        }
-    }
-    return bb;
-};
+
 async function cargadatos(){
-    const resData = await fetch("https://iatacodes-iatacodes-v1.p.rapidapi.com/api/v5/airports", {
+    const resData = await fetch("https://restcountries-v1.p.rapidapi.com/all", {
 	    "method": "GET",
 	    "headers": {
-		"x-rapidapi-host": "leopieters-iata-and-icao-v1.p.rapidapi.com",
-	    "x-rapidapi-key": "3bccf248c4msh3d4a9dedc2c020cp1a889bjsn31014b11cae5"
+	"x-rapidapi-host": "restcountries-v1.p.rapidapi.com",
+	"x-rapidapi-key": "3bccf248c4msh3d4a9dedc2c020cp1a889bjsn31014b11cae5"
 	    }
     });
 
     let data = await resData.json();
     let data_ploty=[];
     let countries=[];
-    let aeropuertos=[];
+    let poblacion=[];
     data.forEach(element => {
-        drugs.push(element.name);
-        dosis.push(element[""]);
+        countries.push(element.name);
+        poblacion.push(element["population"]);
     });
     data_ploty.push({
         histfunc: "sum",
         y: dosis,
         x: drugs,
         type: "histogram",
-        name: "Dosis de drogas"
+        name: "Poblacion de paises"
     });
     Plotly.newPlot('myDiv', data_ploty);
 }
@@ -42,9 +35,9 @@ async function cargadatos(){
 <script src='https://cdn.plot.ly/plotly-latest.min.js' on:load="{cargadatos}"></script>
 </svelte:head>
 <main>
-<h1>Aerolíneas</h1>
+<h1>Poblacion por países</h1>
 <div>
-    En esta gráfica se representan las dosis de diversas drogas
+    En esta gráfica se representan las poblaciones de varios países
 <br>
 <div id="myDiv"></div>
 </div>
